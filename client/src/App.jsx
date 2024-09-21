8873609232f6a3c31742eb172dc26a13f60322c9
Adrin Jose C T <adrinjosect@gmail.com>
2024-09-21 16:55:57 +0530

    Added client framework

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+  import PostJobComponent from "./PostJobComponent";
+
+function App() {
+  return (
+    <>
+      <PostJobComponent />
+    </>
+  );
+}
+
+export default App;
