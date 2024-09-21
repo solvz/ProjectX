d270cddd4a178ebf77bb7990e19a48bf2d70370b
solvz <krishnarajeev2005@gmail.com>
2024-09-21 18:41:42 +0530

    Updated file name

--- contracts/FreelanceMarketplace.sol
+++ contracts/FreelanceMarketplace.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract EscrowPayment {
+contract FreelanceMarketplace {
     struct Job {
         uint256 id;
         address employer;
